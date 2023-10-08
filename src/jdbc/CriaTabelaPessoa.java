f0360dd088ee532117788d4586062a058f6f362a
Cleverson <cleverson.silva@msn.com>
2023-10-08 13:06:10 -0400

    .

---
+++ src/jdbc/CriaTabelaPessoa.java
@@ -0,0 +1,22 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CriaTabelaPessoa {
+	public static void main(String[] args) throws SQLException {
+		Connection conexao = FabricaDeConexao.getConexao();
+
+        String sql = "CREATE TABLE IF NOT EXISTS pessoas ("
+                + "codigo INT AUTO_INCREMENT PRIMARy KEY,"
+                + "nome VARCHAR(80) NOT NULL" + ")";
+
+        Statement stmt = conexao.createStatement();
+        stmt.execute(sql);
+
+        System.out.println("Tabela criada com sucesso!");
+        conexao.close();
+	}
+
+}
