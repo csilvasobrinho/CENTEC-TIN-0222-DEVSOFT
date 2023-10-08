1918134eced710ff94125ae1bdf795bb74023d3d
Cleverson <cleverson.silva@msn.com>
2023-10-08 13:01:42 -0400

    .

---
+++ src/jdbc/FabricaDeConexao.java
@@ -0,0 +1,20 @@
+package jdbc;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaDeConexao {
+	public static Connection getConexao() {
+		try {
+			final String url = "jdbc:mysql://localhost";
+			final String usuario = "root";
+			final String senha = "12345678";
+
+			return DriverManager.getConnection(url, usuario, senha);
+		}catch (SQLException e){
+			throw new RuntimeException(e);
+		}
+	}
+}
