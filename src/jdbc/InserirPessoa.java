90eebe27d90e80ee6069d01a2a2d4ff55efda0e4
Cleverson <cleverson.silva@msn.com>
2023-10-08 15:34:04 -0400

    .

--- src/jdbc/InserirPessoa.java
+++ src/jdbc/InserirPessoa.java
@@ -5,23 +5,24 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class ExcluiPessoa {
+public class InserirPessoa {
 	public static void main(String[] args) throws SQLException {
 		Scanner entrada = new Scanner(System.in);
 		
-		System.out.println("Excluir que código: ");
-		int codigo = entrada.nextInt();
+		System.out.println("Nome: ");
+		String nome = entrada.nextLine();
 		
 		Connection conexao = FabricaDeConexao.getConexao();
-		String sql = "DELETE FROM pessoas WHERE codigo = ?";
+		
+		String sql = "INSERT INTO pessoas (nome) VALUES (?)";
 		
 		PreparedStatement stmt = conexao.prepareStatement(sql);
-		stmt.setInt(1, codigo);
+		stmt.setString(1, nome);
 		
-		if(stmt.executeUpdate() > 0) {
-			System.out.println("Exclusão efetivada!");
-		}else {
-			System.out.println("Exclusão NÃO efetivada!");
-		}
+		stmt.execute();
+		
+		System.out.println("\nINSERT efetivado!");
+		
+		entrada.close();
 	}
 }
