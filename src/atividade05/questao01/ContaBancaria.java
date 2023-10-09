9347da8cd7596c95f594b9bd837ce0857d341308
Cleverson <cleverson.silva@msn.com>
2023-10-08 23:31:58 -0400

    .

--- src/atividade05/questao01/ContaBancaria.java
+++ src/atividade05/questao01/ContaBancaria.java
@@ -2,9 +2,39 @@ package atividade05.questao01;
 
 public class ContaBancaria {
 	
-	String conta;
-	double saldo;
+	private String conta = "102030-40";
+	private double saldo = 1000;
 	
+	public ContaBancaria(String conta, double saldo) {
+		super();
+		this.conta = conta;
+		this.saldo = saldo;
+	}
+
+	public String getConta() {
+		return conta;
+	}
+
+
+
+	public void setConta(String conta) {
+		this.conta = conta;
+	}
+
+
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+
+
 	public void extrato() {
 		System.out.println(conta);
 		System.out.println(saldo);
--- src/atividade05/questao01/TestarConta.java
+++ src/atividade05/questao01/TestarConta.java
@@ -5,12 +5,26 @@ import java.util.Scanner;
 public class TestarConta {
 	public static void main(String[] args) {
 		
-		ContaBancaria cc = new ContaBancaria();
-		
 		Scanner lerValor = new Scanner(System.in);
 		
-		System.out.println("Operação: [1] = Saque;");
-		System.out.println("Operação: [2] = Depósito;");
+		System.out.println("OPERAÇÕES:\n[1] = Saque;\n[2] = Depósito;\n[3] = Extrato.");
+		int opcao = lerValor.nextInt();
+		
+		switch (opcao) {
+		case 1: 
+			ContaBancaria c1 = new ContaBancaria("102030-40", 1000);
+			System.out.println(c1.setConta(null));
+			System.out.println("Operação SAQUE em desenvovimento...");
+			break;
+		case 2: 
+			System.out.println("Operação DEPÓSITO em desenvovimento...");
+			break;
+		case 3: 
+			System.out.println("Operação EXTRATO em desenvovimento...");
+			break;
+		default:
+			System.out.println("Opção não definida!");
+		}
 		
 		lerValor.close();
 	}
