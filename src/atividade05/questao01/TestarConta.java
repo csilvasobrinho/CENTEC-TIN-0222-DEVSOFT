de012ecb07ef346d7e339eb74f489f8c45159d34
Cleverson <cleverson.silva@msn.com>
2023-10-10 10:53:51 -0400

    .

--- src/atividade05/questao01/TestarConta.java
+++ src/atividade05/questao01/TestarConta.java
@@ -6,6 +6,7 @@ public class TestarConta {
 	public static void main(String[] args) {
 		
 		double valor = 0;
+		int saida = 1;
 		Scanner lerOpcao = new Scanner(System.in);
 		Scanner lerValor = new Scanner(System.in);
 		ContaBancaria c1 = new ContaBancaria();
@@ -16,17 +17,25 @@ public class TestarConta {
 		int opcao = lerOpcao.nextInt();
 		
 		switch (opcao) {
-		case 1: 
-			System.out.printf("\n ==> VALOR DO SAQUE: ");
-			valor = lerValor.nextDouble();
-			c1.saque(valor);
-			System.out.println("\n ==> SALDO PÓS SAQUE: " + c1.getSaldo());
+		case 1:
+			while(saida == 1) {
+				System.out.printf("\n ==> VALOR DO SAQUE: ");
+				valor = lerValor.nextDouble();
+				c1.saque(valor);
+				System.out.println("\n ==> SALDO PÓS SAQUE: " + c1.getSaldo());
+				System.out.println("\nDeseja realizar outro saque? [1] Sim ou [2] Não.");
+				saida = lerOpcao.nextInt();
+			}
+			System.out.println("\nGrato pelo saque...!");
 			break;
 		case 2: 
-			System.out.printf("\n ==> VALOR DO DEPÓSITO: ");
-			valor = lerValor.nextDouble();
-			c1.deposito(valor);
-			System.out.println("\n ==> SALDO PÓS DEPÓSITO: " + c1.getSaldo());
+			while(saida ==1) {
+				System.out.printf("\n ==> VALOR DO DEPÓSITO: ");
+				valor = lerValor.nextDouble();
+				c1.deposito(valor);
+				System.out.println("\n ==> SALDO PÓS DEPÓSITO: " + c1.getSaldo());
+			}
+			System.out.println("\nGrato pelo depósito...!");
 			break;
 		case 3: 
 			c1.extrato();;
