64b17aec9426e0892d984618c4811587744264fa
Cleverson <cleverson.silva@msn.com>
2023-10-10 10:46:33 -0400

    .

---
+++ src/atividade05/questao02/Calculadora.java
@@ -0,0 +1,11 @@
+package atividade05.questao02;
+
+public class Calculadora {
+	private double n1 = 0;
+	private double n2 = 0;
+	
+	public double soma(double valor1, double valor2) {
+		return 0;
+	}
+	
+}
